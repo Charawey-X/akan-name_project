24039efbeccab16dd5f5057852b7e3dd8adcbb3f
Charawey-X <charawey@gmail.com>
2022-03-07 08:50:53 +0300

    Edited file

--- main.js
+++ main.js
@@ -1,69 +1,56 @@
-//html linking:
-let form = document.getElementById("form");
-let my-date = document.getElementById("date");
-let my-month = document.getElementById("month");
-let my-year = document.getElementById("year");
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let yourday =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+function getBirthday() {
+    let dateInput = document.getElementById("date").value;
+    let date = new Date(dateInput);
 
+    if (!!date.valueOf()) {
+        let year = date.getFullYear().toString();
+        // let MM = date.getMonth()+1;
+        // let DD = date.getDate();
+        // let CC = parseInt(year.slice(0,2));
+        // let YY = parseInt(year.slice(2,4));
+        // let dayOfWeek = (((CC / 4) - 2 * CC - 1) + (5 * YY / 4) + (26 * (MM + 1) / 10) + DD) % 7;
+        let dayOfWeek = date.getDay();
+        return dayOfWeek;
+    } else {
+        alert("Date is invalid.");
+        return;
+    }
+}
 
-let akan-name = document.querySelector(".akanname");
-
-
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let userGender = form.gender.value;
-
-//calculating my-day:
-    if (my-date.value === ""){
-        alert("Please enter the day you were born");
-    }else{
-        if(my-date.value <= 0 || my-date.value >31){
-            alert("Enter a valid date")
+function getGender() {
+    let gender;
+    if ((form.gender[0].checked == true) || (form.gender[1].checked == true)) {
+        if (form.gender[0].checked == true) {
+            gender = 0;
+        } else if (form.gender[1].checked == true) {
+            gender = 1;
         }
+        return gender;
+    } else {
+        alert("Please select gender.");
+        return;
     }
+}
 
-    if (my-month.value === ""){
-        alert("Please enter the Month you were born");
-    }else{
-        if(my-month.value <= 0 || my-month.value >12){
-            alert("Enter a valid Month")
+function computeName() {
+    let selectedGender = getGender();
+    let dayOfWeek = getBirthday();
+    if ((!!dayOfWeek.valueOf() === true) || (dayOfWeek == 0))  {
+        if (selectedGender === 0) {
+            let result = "Your day is " + yourday[dayOfWeek] + "." + "\ Your Akan name is " + maleNames[dayOfWeek] + ".";
+            document.getElementById("result").innerHTML = result;
+        } else if (selectedGender === 1) {
+            let result = "Your  day is " + yourday[dayOfWeek] + "." + "\ Your Akan name is " +  femaleNames[dayOfWeek] + ".";
+            document.getElementById("result").innerHTML = result;
         }
-    }
-    if (my-year.value === ""){
-        alert("Please enter the Year you were born");
     }else{
-        if(theYear.value <= 0000 || theYear.value >2100){
-            alert("Enter a valid year between 0000 and 2100")
-        }
+        return;
     }
-    if (userGender === ""){
-        alert("Please check your gender");
-    }
-
-
-
-
-    var akanname = compareNames(theYear.value, theMonth.value, theDay.value, userGender)
-
-    akan-name.innerHTML = `Your Akan name is: ${akanname}`;
-});
-
-
-function compareNames(year, month, day, gender){
-    var wkday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-
-    if (gender === "male"){
-        var date = new Date(parseInt(year), (parseInt(month)-1), parseInt(day))
-
-        var dayofweek = wkday[date.getDay()];
-
-     return male[date.getDay()]
-    }else{
-        var date = new Date(parseInt(year), (parseInt(month)-1), parseInt(day))
-        var dayofWeek = wkday[date.getDay()];
-        return female[date.getDay()]
-    }
-
+}
+//Clear output when date of birth or gender is changed.
+function clearOutput(){
+    document.getElementById("result").innerHTML = " ";
 }
